65c3cb78ddd9d46ddd962b31da37a36ccd1c8e89
Justin Miller <justin.miller@logrhythm.com>
2016-10-14 11:15:27 -0600

    Feature: Small fixes.
    * Add range voting
    * Add in the range style picking.
    * Update message sent to all.
    * Update vote to be random per user.
    * Update the voteResult to tell me of ties

--- sendMail.js
+++ sendMail.js
@@ -7,13 +7,11 @@ const crypt = new Encrypt(privateKey);
 
 const helper = sendgrid.mail;
 const from_email = new helper.Email('justin.miller@logrhythm.com');
-const templateContent = (emailAddress) => `
-<p>Hello,</p>
-  <p>Now is your chance to vote, but I don't want the issues of <a href="https://en.wikipedia.org/wiki/First-past-the-post_voting">first past the post</a>. To solve it the best, we have a <a href="https://en.wikipedia.org/wiki/Schulze_method">schulze voting method</a>.</p>
-  </br>
-   <p><a href="http://algorithmalchemist.net/?id=${crypt.encrypt(emailAddress, 'base64')}">Link to Your voting page, your id is unique</a> <a href="http://ec2-52-40-203-50.us-west-2.compute.amazonaws.com/?id=${crypt.encrypt(emailAddress, 'base64')}"> Alt Link </a></p>
-   <p><a href="http://algorithmalchemist.net/getResults">View Ranked Results</a> ------------------ <a href="http://algorithmalchemist.net/getVotes"> Raw Votes </a></p>
-   <p><a href="https://followmyvote.com/majority-voting-systems/">Majority Voting Systems</a> Web article on voting.</p>
+const templateContent = (emailAddress) => `<p>Hello,</p><p>
+My hackathon was to fix the problem with our voting, also known as <a href="https://en.wikipedia.org/wiki/First-past-the-post_voting">first past the post</a>. So I have elected to use a approval method for casting the vote, and to determine the votes via the <a href="https://en.wikipedia.org/wiki/Schulze_method">schulze method</a> or the <a href="https://en.wikipedia.org/wiki/Range_voting">range voting system</a>. I like the range voting the best, but both methods are better than <a href="https://en.wikipedia.org/wiki/Instant-runoff_voting">instant run off</a> and <a href="https://en.wikipedia.org/wiki/First-past-the-post_voting">first past the post</a>.</p>
+<p>So please, take the chance to <a href="http://algorithmalchemist.net/?id=${crypt.encrypt(emailAddress, 'base64')})">cast your vote</a>, or <a href="http://ec2-52-40-203-50.us-west-2.compute.amazonaws.com/?id=${crypt.encrypt(emailAddress, 'base64')})">alternative link(alternative)</a>.
+Then you can go to <a href="http://algorithmalchemist.net/getResults">results</a>, or if you want the <a href="http://algorithmalchemist.net/getResults">raw results</a>.</p>
+<p>For more information on voting, I would look into <a href="https://followmyvote.com/majority-voting-systems/">Majority Voting Systems</a>, which references ccgray’s videos on voting.</p>
 `;
 
 const sendMail = (emailAddress) => {
